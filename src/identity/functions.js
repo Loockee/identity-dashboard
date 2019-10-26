ec5060e2034fa1e603eb2d88159dc90ec7fff211
ollie314 <mehdi.lefebvre@gmail.com>
2019-10-26 06:53:43 -0400

    fix: adjust the way of working with fetch
    Signed-off-by: ollie314 <mehdi.lefebvre@gmail.com>

--- src/identity/functions.js
+++ src/identity/functions.js
@@ -1,6 +1,8 @@
 import uuid from "uuid";
 
-const createMethod = ({ type, ...rest }) => ({ ...rest, method: type });
+const setMode = ({...rest}) => ({...rest, mode: 'cors'});
+const setCache = ({...rest}) => ({...rest, cache: 'no-cache'})
+const createMethod = ({ type, ...rest }) => ({ ...rest, method: type.toUpperCase() });
 const createPayload = ({ payload, ...rest }) => ({
   ...rest,
   body: payload ? JSON.stringify(payload) : ""
@@ -14,10 +16,10 @@ const createHeaders = ({ headers, ...rest }) => ({
     "x-sender-id": "react-tyne-0.0.1"
   }
 });
+const toHeaders = ({headers, ...rest}) => ({...rest, headers:new Headers(headers)});
 
 /// will be better to use a promise based pipeline since some process requires asynchronoucity
 const pipeline = (type, payload) => {
-  // [createPayload, createHeaders, createMethod]
   function t(steps, input) {
     if (steps.length) {
       const o = steps.shift().apply(this, [input]);
@@ -25,36 +27,46 @@ const pipeline = (type, payload) => {
     }
     return input;
   }
-  return t([createPayload, createHeaders, createMethod], { type, payload });
+  return t([setMode, setCache, createMethod, createPayload, createHeaders, toHeaders], { type, payload });
 };
 
 export function createRequest(type, url, payload) {
-  // TODO: integrate oAuth handsaker here
-  let req = null;
-  const { method, body, headers } = pipeline(type, payload); // TODO: complete;
-  req = new Request(url, { method, body, headers });
-  return new Request(url, req);
+  // TODO: integrate oAuth handshaker here
+  const { mode, method, body, headers, cache } = pipeline(type, payload);
+  console.log(headers, mode);
+  return new Request(url, { method, body, headers, mode, cache });
 }
 
 export async function sendRequest(request) {
-  // the way to previde interceptors and some stuff like that
-  // ffor now, we are only supporting text as body (use json to map fetch response)
-  console.log("sending request", request);
-  return Promise.resolve(true);
-  /*
+  // this is our way to provide interceptors and some stuff like that.
+  // For now, we are only supporting text as body (use json to map fetch response).
+  // We are not allowing external definition of interception
   // Check if we have to map that in a fetch
-  // use async/await for implementation
+  // TODO: use async/await for implementation
   fetch(request)
-    .then(response => response.json)
-    // perform others interceptions steps
-    .then(() => {
-      console.log("posted");
+    // logging interception
+    .then((response) => {
+      console.log("response received");
+      return response;
     })
+    // Error catching
+    .then((response) => {
+      // TODO: extends error management
+      if(!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response;
+    })
+    // Payload transformation
+    .then(response => response.json())
+    // perform others interceptions steps
+    // ...
     // catch errors
-    .catch(() => {
-      console.log("an error occurred during the saving process");
+    .catch((error) => {
+      // TODO: mvoe that in a specific logging functions stack
+      const log  = console.error ? console.error : console.log
+      log("an error occurred during the saving process", error);
     });
-    */
 }
 
 let connected = false; // to bypass the bug due to my missunderstanding of hooks and react lifecycle
